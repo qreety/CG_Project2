b12a81c0ed722b38c43c20393351a377cd6937c0
qreety <qreety@gmail.com>
2016-03-12 02:00:04 -0600

    2nd attempt
    Failed.
    Nothin shows up

--- CG_2/main.cpp
+++ CG_2/main.cpp
@@ -5,6 +5,7 @@
 *	anttweakbar.sourceforge.net
 */
 #define _CRT_SECURE_NO_DEPRECATE
+#define BUFFER_OFFSET(offset) ((GLvoid*) (NULL + offset))
 
 #include <GL\glew.h>
 #include <GL\freeglut.h>
@@ -16,43 +17,64 @@
 #include <vector>
 #include <AntTweakBar.h>
 
-<<<<<<< HEAD
-#define BUFFER_OFFSET(offset) ((GLvoid*) (NULL + offset))
-=======
 GLuint NumTris, material_count;
-GLfloat x_min = FLT_MIN, x_max = FLT_MIN, 
-		y_min = FLT_MIN, y_max = FLT_MIN, 
-		z_min = FLT_MIN, z_max = FLT_MIN; //To calculate the model position
+GLfloat x_min = FLT_MIN, x_max = FLT_MIN,
+y_min = FLT_MIN, y_max = FLT_MIN,
+z_min = FLT_MIN, z_max = FLT_MIN; //To calculate the model position
 GLfloat global_r, global_g, global_b;
-GLint model = 2, ori, sm, rotate, display_type = 2;
+GLint model = 2, ori, sm, rotate;
 TwBar *bar;
 GLfloat width, height;
 GLboolean dLightr;
 GLuint programID;
 GLuint vertexarray, vertexbuffer, normalbuffer, mbuffer;
->>>>>>> parent of f5f9fa1... Ver 1.3
 
-enum VAO_IDs{Triangles, NumVAOs};
-enum Buffer_IDs{ArrayBuffer, NumBuffers};
-enum Attrib_IDs{vPosition = 0};
+std::vector<glm::vec3> vertices;
+std::vector<glm::vec3> normals;
+std::vector<int> mindices;
+
+typedef enum { MODEL_CUBE, MODEL_COW, MODEL_PHONE } Model;
+typedef enum { CCW, CW } Orientation; // default front-facing is CCW
+typedef enum { FLAT, SMOOTH } Shading;
+typedef enum { X, Y, Z } Rotation;
+
+enum VAO_IDs { Triangles, NumVAOs};
+enum Buffer_IDs { ArrayBuffer, NumBuffer};
+enum Attrib_IDs { vPosition };
 
 GLuint VAOs[NumVAOs];
-GLuint Buffers[NumBuffers];
+GLuint Buffers[NumBuffer];
 const GLuint NumVert = 6;
 GLuint programID;
 
-void init(void);
+typedef struct{
+	GLfloat x, y, z;
+	GLfloat nx, ny, nz;
+	GLuint m;
+} vertex;
 
-void RenderFunction(void){
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+typedef struct{
+	GLfloat x, y, z;
+} svertex;
 
-	glBindVertexArray(VAOs[Triangles]);
+typedef struct{
+	glm::vec3 ambient;
+	glm::vec3 diffuse;
+	glm::vec3 specular;
+	GLfloat shine;
+} material;
 
-	glDrawArrays(GL_TRIANGLES, 0, NumVert);
+typedef struct{
+	glm::vec3 ambient;
+	glm::vec3 diffuse;
+	glm::vec3 specular;
+	glm::vec4 position;
+	GLboolean on;
+} light;
 
-<<<<<<< HEAD
-	glFlush();
-=======
+material *Mate;
+light sLight, dLight;
+
 void Initialize(int, char*[]);
 void InitializeUI(void);
 void ResizeFunction(int, int);
@@ -61,8 +83,43 @@ void ExitFuntion(int);
 void TW_CALL Reset(void *clientDate);
 void LoadTriangle(void);
 void CreateGeometry(void);
-void SetLights(glm::vec3, glm::vec3);
-void SetUniform(int, glm::vec3, glm::mat4, glm::mat4, glm::mat4, light, light);
+//void SetLights(glm::vec3, glm::vec3);
+//void SetUniform(int, glm::vec3, glm::mat4, glm::mat4, glm::mat4, light, light);
+
+/////////////Test VAO, VBO
+void display(void){
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glBindVertexArray(VAOs[Triangles]);
+	glDrawArrays(GL_TRIANGLES, 0, NumVert);
+	glFlush();
+}
+
+void CreateGeometry(){
+	glGenVertexArrays(1, VAOs);
+	glBindVertexArray(VAOs[Triangles]);
+
+	GLfloat vertice[NumVert][2] = {
+		{ -0.9f, -0.9f  },
+		{0.85f, -0.9f },
+		{-0.9f, 0.85f},
+		{0.9f, -0.85f},
+		{0.9f, 0.9f},
+		{-0.85f, 0.9f}
+		};
+
+	glGenBuffers(1, Buffers);
+	glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertice), vertice, GL_STATIC_DRAW);
+	glVertexAttribPointer(
+		0,
+		2,
+		GL_FLOAT,
+		GL_FALSE,
+		0,
+		GL_TEXTURE_BUFFER_OFFSET(0)
+		);
+	glEnableVertexAttribArray(0);
+}
 
 //Return the minimum value of three values
 GLfloat min3(GLfloat x, GLfloat y, GLfloat z){
@@ -107,49 +164,49 @@ bool readin(char *FileName) {
 	while (ch != '\n') // skip documentation line
 		fscanf(fp, "%c", &ch);
 
-	Tris = new triangle[NumTris];
-	for (int i = 0; i<NumTris; i++) // read triangles
-	{
-		fscanf(fp, "v0 %f %f %f %f %f %f %d\n",
-			&(Tris[i].v0.x), &(Tris[i].v0.y), &(Tris[i].v0.z),
-			&(Tris[i].normal[0].x), &(Tris[i].normal[0].y), &(Tris[i].normal[0].z),
-			&(Tris[i].Color[0]));
-		fscanf(fp, "v1 %f %f %f %f %f %f %d\n",
-			&(Tris[i].v1.x), &(Tris[i].v1.y), &(Tris[i].v1.z),
-			&(Tris[i].normal[1].x), &(Tris[i].normal[1].y), &(Tris[i].normal[1].z),
-			&(Tris[i].Color[1]));
-		fscanf(fp, "v2 %f %f %f %f %f %f %d\n",
-			&(Tris[i].v2.x), &(Tris[i].v2.y), &(Tris[i].v2.z),
-			&(Tris[i].normal[2].x), &(Tris[i].normal[2].y), &(Tris[i].normal[2].z),
-			&(Tris[i].Color[2]));
-		fscanf(fp, "face normal %f %f %f\n", &(Tris[i].face_normal.x), &(Tris[i].face_normal.y),
-			&(Tris[i].face_normal.z));
+	//Tris = new triangle[NumTris];
+	//for (int i = 0; i<NumTris; i++) // read triangles
+	//{
+	//	fscanf(fp, "v0 %f %f %f %f %f %f %d\n",
+	//		&(Tris[i].v0.x), &(Tris[i].v0.y), &(Tris[i].v0.z),
+	//		&(Tris[i].normal[0].x), &(Tris[i].normal[0].y), &(Tris[i].normal[0].z),
+	//		&(Tris[i].Color[0]));
+	//	fscanf(fp, "v1 %f %f %f %f %f %f %d\n",
+	//		&(Tris[i].v1.x), &(Tris[i].v1.y), &(Tris[i].v1.z),
+	//		&(Tris[i].normal[1].x), &(Tris[i].normal[1].y), &(Tris[i].normal[1].z),
+	//		&(Tris[i].Color[1]));
+	//	fscanf(fp, "v2 %f %f %f %f %f %f %d\n",
+	//		&(Tris[i].v2.x), &(Tris[i].v2.y), &(Tris[i].v2.z),
+	//		&(Tris[i].normal[2].x), &(Tris[i].normal[2].y), &(Tris[i].normal[2].z),
+	//		&(Tris[i].Color[2]));
+	//	fscanf(fp, "face normal %f %f %f\n", &(Tris[i].face_normal.x), &(Tris[i].face_normal.y),
+	//		&(Tris[i].face_normal.z));
 
 		//Get the minimum and maximum value to calculate the position
-		x_temp = min3(Tris[i].v0.x, Tris[i].v1.x, Tris[i].v2.x);
-		y_temp = min3(Tris[i].v0.y, Tris[i].v1.y, Tris[i].v2.y);
-		z_temp = min3(Tris[i].v0.z, Tris[i].v1.z, Tris[i].v2.z);
-		x_min = x_min < x_temp ? x_min : x_temp;
-		y_min = y_min < y_temp ? y_min : y_temp;
-		z_min = z_min < z_temp ? z_min : z_temp;
+	//	x_temp = min3(Tris[i].v0.x, Tris[i].v1.x, Tris[i].v2.x);
+	//	y_temp = min3(Tris[i].v0.y, Tris[i].v1.y, Tris[i].v2.y);
+	//	z_temp = min3(Tris[i].v0.z, Tris[i].v1.z, Tris[i].v2.z);
+	//	x_min = x_min < x_temp ? x_min : x_temp;
+	//	y_min = y_min < y_temp ? y_min : y_temp;
+	//	z_min = z_min < z_temp ? z_min : z_temp;
 
-		x_temp = max3(Tris[i].v0.x, Tris[i].v1.x, Tris[i].v2.x);
-		y_temp = max3(Tris[i].v0.y, Tris[i].v1.y, Tris[i].v2.y);
-		z_temp = max3(Tris[i].v0.z, Tris[i].v1.z, Tris[i].v2.z);
-		x_min = x_min > x_temp ? x_min : x_temp;
-		y_min = y_min > y_temp ? y_min : y_temp;
-		z_min = z_min > z_temp ? z_min : z_temp;
+	//	x_temp = max3(Tris[i].v0.x, Tris[i].v1.x, Tris[i].v2.x);
+	//	y_temp = max3(Tris[i].v0.y, Tris[i].v1.y, Tris[i].v2.y);
+	//	z_temp = max3(Tris[i].v0.z, Tris[i].v1.z, Tris[i].v2.z);
+	//	x_min = x_min > x_temp ? x_min : x_temp;
+	//	y_min = y_min > y_temp ? y_min : y_temp;
+	//	z_min = z_min > z_temp ? z_min : z_temp;
 
-		vertices.push_back(Tris[i].v0);
-		normals.push_back(Tris[i].normal[0]);
-		mindices.push_back(Tris[i].Color[0]);
-		vertices.push_back(Tris[i].v1);
-		normals.push_back(Tris[i].normal[1]);
-		mindices.push_back(Tris[i].Color[1]);
-		vertices.push_back(Tris[i].v2);
-		normals.push_back(Tris[i].normal[2]);
-		mindices.push_back(Tris[i].Color[2]);
-	}
+	//	vertices.push_back(Tris[i].v0);
+	//	normals.push_back(Tris[i].normal[0]);
+	//	mindices.push_back(Tris[i].Color[0]);
+	//	vertices.push_back(Tris[i].v1);
+	//	normals.push_back(Tris[i].normal[1]);
+	//	mindices.push_back(Tris[i].Color[1]);
+	//	vertices.push_back(Tris[i].v2);
+	//	normals.push_back(Tris[i].normal[2]);
+	//	mindices.push_back(Tris[i].Color[2]);
+	//}
 
 	fclose(fp);
 	return true;
@@ -158,62 +215,48 @@ bool readin(char *FileName) {
 int main(int argc, char **argv)
 {
 	Initialize(argc, argv);
-	InitializeUI();
+	//InitializeUI();
 
 	glutReshapeFunc(ResizeFunction);
-	glutDisplayFunc(RenderFunction);
-	glutIdleFunc(RenderFunction);
+	glutDisplayFunc(display);
+	glutIdleFunc(display);
 
 	glewInit();
-	programID = LoadShaders("shader.vert", "shader.frag");
-		
+	programID = LoadShaders("shader.vert", "testShader.frag");
+	CreateGeometry();
+
 	glutMainLoop();
-	TwTerminate();
->>>>>>> parent of f5f9fa1... Ver 1.3
+	//TwTerminate();
 }
 
-int main(int argc, char** argv){
+void Initialize(int argc, char **argv){ // To initialize GLUT and window
+
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL);
-	glutInitWindowSize(800, 600);
-<<<<<<< HEAD
-	glutInitContextVersion(4, 3);
+	glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
 	glutInitContextProfile(GLUT_CORE_PROFILE);
-	glutCreateWindow("Assignment II");
-=======
+
+	glutInitWindowSize(800, 600);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
 
 	glutCreateWindow("Project II");
 
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
-	switch (sm)
-	{
-	case 0:
-		glShadeModel(GL_FLAT);
-		break;
-	case 1:
-		glShadeModel(GL_SMOOTH);
-		break;
-	default:
-		glShadeModel(GL_FLAT);
-		break;
-	}
-	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-	glReadBuffer(GL_BACK);
-	glDrawBuffer(GL_BACK);
-	glEnable(GL_CULL_FACE);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	//glReadBuffer(GL_BACK);
+	//glDrawBuffer(GL_BACK);
+	//glEnable(GL_CULL_FACE);
 
 	// Initialize AntTweakBar
-	TwInit(TW_OPENGL, NULL);
+	//TwInit(TW_OPENGL, NULL);
 
 	// Set GLUT event callbacks
-	glutMouseFunc((GLUTmousebuttonfun)TwEventMouseButtonGLUT);
-	glutMotionFunc((GLUTmousemotionfun)TwEventMouseMotionGLUT);
-	glutPassiveMotionFunc((GLUTmousemotionfun)TwEventMouseMotionGLUT);
-	glutKeyboardFunc((GLUTkeyboardfun)TwEventKeyboardGLUT);
-	glutSpecialFunc((GLUTspecialfun)TwEventSpecialGLUT);
-	TwGLUTModifiersFunc(glutGetModifiers);
+	//glutMouseFunc((GLUTmousebuttonfun)TwEventMouseButtonGLUT);
+	//glutMotionFunc((GLUTmousemotionfun)TwEventMouseMotionGLUT);
+	//glutPassiveMotionFunc((GLUTmousemotionfun)TwEventMouseMotionGLUT);
+	//glutKeyboardFunc((GLUTkeyboardfun)TwEventKeyboardGLUT);
+	//glutSpecialFunc((GLUTspecialfun)TwEventSpecialGLUT);
+	//TwGLUTModifiersFunc(glutGetModifiers);
 }
 
 void InitializeUI(){
@@ -324,7 +367,7 @@ void RenderFunction(){
 	glm::mat4 ViewMatrix;
 	glm::mat4 ModelMatrix = glm::mat4(1.0f);
 	glm::mat4 MVPMatrix;
-	
+
 	glm::vec3 camPos;
 	glm::vec3 objPos;
 
@@ -334,21 +377,59 @@ void RenderFunction(){
 	//Perspective
 	GLfloat aspect = (GLfloat)width / (GLfloat)height;
 	glViewport(0, 0, (GLsizei)width, (GLsizei)height);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
+	
 	//gluPerspective(45.0, aspect, 1.0, 3000.0);
->>>>>>> parent of f5f9fa1... Ver 1.3
 
-	glutDisplayFunc(RenderFunction);
+	ProjectionMatrix = glm::perspective(45.0f, 4.0f / 3.0f, 1.0f, 1000.0f);
 
-	glewInit();
+	if (ori == 0)
+		glFrontFace(GL_CCW);
+	else if (ori == 1)
+		glFrontFace(GL_CW);
 
-	programID = LoadShaders("shader.vert", "testShader.frag");
+	switch (model)
+	{
+	case 0:
+		//gluLookAt(0, 0, 1, 0, 0, -2, 0, 1, 0);
+		ViewMatrix = glm::lookAt(glm::vec3(0.0f, 1.0f, 0.0f),
+			glm::vec3(0.0f, 0.0f, -2.0f),
+			glm::vec3(0.0f, 1.0f, 0.0f));
+		camPos = { 0.0f, 1.0f, 0.0f };
+		break;
+	case 1:
+		//gluLookAt((x_min + x_max) / 2, (y_min + y_max) / 2, 800 + (z_min + z_max) / 2, (x_min + x_max) / 2, (y_min + y_max) / 2, (z_min + z_max) / 2, 0, 1, 0);
+		ViewMatrix = glm::lookAt(glm::vec3((x_min + x_max) / 2, (y_min + y_max) / 2, 800 + (z_min + z_max) / 2),
+			glm::vec3((x_min + x_max) / 2, (y_min + y_max) / 2, (z_min + z_max) / 2),
+			glm::vec3(0.0f, 1.0f, 0.0f));
+		camPos = { (x_min + x_max) / 2, (y_min + y_max) / 2, 800 + (z_min + z_max) / 2 };
+		break;
+	case 2:
+		//gluLookAt((x_min + x_max) / 2, (y_min + y_max) / 2 - 30, 400 + (z_min + z_max) / 2, (x_min + x_max) / 2, (y_min + y_max) / 2, (z_min + z_max) / 2, 0, 1, 0);
+		glTranslatef(80, 100, 0);
+		ViewMatrix = glm::lookAt(glm::vec3((x_min + x_max) / 2 + 80, (y_min + y_max) / 2 + 70, 400 + (z_min + z_max) / 2),
+			glm::vec3((x_min + x_max) / 2 + 80, (y_min + y_max) / 2 + 100, (z_min + z_max) / 2),
+			glm::vec3(0.0f, 1.0f, 0.0f));
+		camPos = { (x_min + x_max) / 2 + 80, (y_min + y_max) / 2 + 70, 400 + (z_min + z_max) / 2 };
+		break;
+	}
 	glUseProgram(programID);
+	glBindVertexArray(vertexarray);
 
-<<<<<<< HEAD
-	init();
-=======
+	MVPMatrix = ProjectionMatrix * ViewMatrix * ModelMatrix;
+	//
+	SetLights(camPos, objPos);
+	SetUniform(programID, camPos, ModelMatrix, ViewMatrix, MVPMatrix, sLight, dLight);
+	glDrawArrays(GL_TRIANGLES, 0, NumTris);
+
+	//assign color to each mesh
+	glColor3f(1.0f, 0.0f, 0.0f);
+	//for (int i = 0; i < NumTris; i++)
+	//{
+	//	glVertex3f(Tris[i].v0.x, Tris[i].v0.y, Tris[i].v0.z);
+	//	glVertex3f(Tris[i].v1.x, Tris[i].v1.y, Tris[i].v1.z);
+	//	glVertex3f(Tris[i].v2.x, Tris[i].v2.y, Tris[i].v2.z);
+	//}
+
 	glEnd();
 	glFlush();
 
@@ -358,7 +439,6 @@ void RenderFunction(){
 	glDisableVertexAttribArray(1);
 	glDisableVertexAttribArray(2);
 
-	glPopMatrix();
 	glutSwapBuffers();
 }
 
@@ -366,7 +446,7 @@ void ResizeFunction(int Width, int Height){
 	glViewport(0, 0, (GLsizei)Width, (GLsizei)Height);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(45.0, 4.0f/3.0f, 1.0f, 3000.0f);
+	gluPerspective(45.0, 4.0f / 3.0f, 1.0f, 3000.0f);
 
 	glMatrixMode(GL_MODELVIEW);
 	glutSwapBuffers();
@@ -387,78 +467,91 @@ void LoadTriangle(){
 	}
 }
 
-void CreateGeometry(){
-	glGenVertexArrays(1, &vertexarray);
-	glBindVertexArray(vertexarray);
+void SetLights(glm::vec3 camPos, glm::vec3 objPos){
+	glm::vec3 dir = objPos - glm::vec3(dLight.position);
+	while (dLightr){
+		// GLUT_ELAPSED_TIME is in milliseconds
+		static int prvMs = glutGet(GLUT_ELAPSED_TIME);
+		const int curMs = glutGet(GLUT_ELAPSED_TIME);
 
-	glGenBuffers(1, &vertexbuffer);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3), &vertices[0], GL_STATIC_DRAW);
-	glVertexAttribPointer(
-		0,
-		3,
-		GL_FLOAT,
-		GL_FALSE,
-		0,
-		(void*)0
-		);
+		// dt is in seconds
+		const double dt = (curMs - prvMs) / 1000.0;
+		prvMs = curMs;
 
-	glGenBuffers(1, &normalbuffer);
-	glEnableVertexAttribArray(1);
-	glBindBuffer(GL_ARRAY_BUFFER, normalbuffer);
-	glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(glm::vec3), &normals[0], GL_STATIC_DRAW);
-	glVertexAttribPointer(
-		1,
-		3,
-		GL_FLOAT,
-		GL_FALSE,
-		0,
-		(void*)0
-		);
+		// update world state
+		float speed = 30.0f;
+		float degree = speed * dt;
 
-	glGenBuffers(1, &mbuffer);
-	glEnableVertexAttribArray(2);
-	glBindBuffer(GL_ARRAY_BUFFER, mbuffer);
-	glBufferData(GL_ARRAY_BUFFER, mindices.size() * sizeof(int), &mindices[0], GL_STATIC_DRAW);
-	glVertexAttribPointer(
-		2,
-		1,
-		GL_UNSIGNED_INT,
-		GL_FALSE,
-		0,
-		(void*)0
-		);
-	glBindVertexArray(0);
->>>>>>> parent of f5f9fa1... Ver 1.3
-
-	glutMainLoop();
+		switch (rotate)
+		{
+		case 0:	//Rotate around X
+			glm::vec3(dLight.position) = glm::mat3(1.0f, 0.0f, 0.0f,
+				0.0f, cos(degree), -sin(degree),
+				0.0f, sin(degree), cos(degree)) * dir + objPos;
+			break;
+		case 1: //Rotate around Y
+			glm::vec3(dLight.position) = glm::mat3(cos(degree), 0.0f, sin(degree),
+				0.0f, 1.0f, 0.0f,
+				-sin(degree), 0.0f, cos(degree)) * dir + objPos;
+			break;
+		case 2: //Rotate around Z
+			glm::vec3(dLight.position) = glm::mat3(cos(degree), -sin(degree), 0.0f,
+				sin(degree), cos(degree), 0.0f,
+				0.0f, 0.0f, 1.0f) * dir + objPos;
+			break;
+		default:
+			break;
+		}
+	}
 }
 
-void init(void){
-		
-	if (glGenVertexArrays == NULL)
-	{
-		printf("GLEW not initialized");
+void SetUniform(int programID, glm::vec3 camPos, glm::mat4 ModelMatrix, glm::mat4 ViewMatrix, glm::mat4	MVPMatrix, light sLight, light dLight){
+	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+	GLuint ViewMatrixID = glGetUniformLocation(programID, "V");
+	GLuint ModelMatrixID = glGetUniformLocation(programID, "M");
+	GLuint aLightID = glGetUniformLocation(programID, "aLight");
+
+	glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVPMatrix[0][0]);
+	glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &ModelMatrix[0][0]);
+	glUniformMatrix4fv(ViewMatrixID, 1, GL_FALSE, &ViewMatrix[0][0]);
+	//Global ambient light
+	glUniform3f(aLightID, global_r, global_g, global_b);
+	//Directional light
+	glUniform4f(glGetUniformLocation(programID, "sLight.position"), 0.0f, -1.f, -1.0f, 0.0f);
+	glUniform3f(glGetUniformLocation(programID, "sLight.ambient"), sLight.ambient.r, sLight.ambient.g, sLight.ambient.b);
+	glUniform3f(glGetUniformLocation(programID, "sLight.diffuse"), sLight.diffuse.r, sLight.diffuse.g, sLight.diffuse.b);
+	glUniform3f(glGetUniformLocation(programID, "sLight.specular"), sLight.specular.r, sLight.specular.g, sLight.specular.b);
+	glUniform1i(glGetUniformLocation(programID, "sLight.on"), sLight.on);
+	//Ponit light
+	glUniform4f(glGetUniformLocation(programID, "dLight.position"), dLight.position.x, dLight.position.y, dLight.position.z, dLight.position.w);
+	glUniform3f(glGetUniformLocation(programID, "dLight.ambient"), dLight.ambient.r, dLight.ambient.g, dLight.ambient.b);
+	glUniform3f(glGetUniformLocation(programID, "dLight.diffuse"), dLight.diffuse.r, dLight.diffuse.g, dLight.diffuse.b);
+	glUniform3f(glGetUniformLocation(programID, "dLight.specular"), dLight.specular.r, dLight.specular.g, dLight.specular.b);
+	glUniform1i(glGetUniformLocation(programID, "dLight.on"), dLight.on);
+	//Materials
+	for (int i = 0; i < material_count; i++){
+		glUniform3f(glGetUniformLocation(programID, "mat[i].ambient"), Mate[i].ambient.r, Mate[i].ambient.g, Mate[i].ambient.b);
+		glUniform3f(glGetUniformLocation(programID, "mat[i].diffuse"), Mate[i].diffuse.r, Mate[i].diffuse.g, Mate[i].diffuse.b);
+		glUniform3f(glGetUniformLocation(programID, "mat[i].specular"), Mate[i].specular.r, Mate[i].specular.g, Mate[i].specular.b);
+		glUniform1f(glGetUniformLocation(programID, "mat[i].shine"), Mate[i].shine);
 	}
+	//Camera position
+	glUniform3f(glGetUniformLocation(programID, "camPos"), camPos.x, camPos.y, camPos.z);
 
-	glGenVertexArrays(NumVAOs, VAOs);
-	glBindVertexArray(VAOs[0]);
+}
 
-	GLfloat vertices[NumVert][2] = {
-		{ -0.9f, -0.9f  },
-		{0.85f, -0.9f },
-		{-0.9f, 0.85f},
-		{0.9f, -0.85f},
-		{0.9f, 0.9f},
-		{-0.85f, 0.9f}
-	};
-
-	glGenBuffers(1, Buffers);
-	glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	
-	glVertexAttribPointer(vPosition, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
-	glEnableVertexAttribArray(vPosition);
+void TW_CALL Reset(void *clientDate){
+	switch (model)
+	{
+	case 0:
+		dLight.position = { 0.0f, 1.0f, 0.0f, 1.0f };
+		break;
+	case 1:
+		dLight.position = { (x_min + x_max) / 2, (y_min + y_max) / 2, 800 + (z_min + z_max) / 2, 1.0f };
+		break;
+	case 2:
+		dLight.position = { (x_min + x_max) / 2 + 80, (y_min + y_max) / 2 + 70, 400 + (z_min + z_max) / 2, 1.0f };
+		break;
+	}
 }
 
--- CG_2/shader.frag
+++ CG_2/shader.frag
@@ -1,85 +1,8 @@
 #version 330 core
-struct Material{
-	vec3 ambient;
-	vec3 diffuse;
-	vec3 specular;
-	float shine;
-};
-
-struct Light{
-	vec4 position;
-	vec3 ambient;
-	vec3 diffuse;
-	vec3 specular;
-	bool on;
-};
-
-int numLight;
-float attenuation;
-Light lights[2];
-//20
-in vec3 vNormal;  //in camera space
-flat in int m;
-in vec3 vPos; // in world space
-in vec4 aColor;
 
 out vec4 color;
 
-uniform vec3 camPos;
-uniform vec3 aLight;
-uniform Light sLight;
-uniform Light dLight;
-uniform Material mat[6];
-
 void main(){
-    if(sLight.on == true && dLight.on == true){
-		lights[0] = sLight;
-		lights[1] = dLight;
-		numLight = 2;
-	}//40
-	else if(sLight.on == true && dLight.on == false){
-		lights[0] = sLight;
-		numLight = 1;
-	}
-	else if(sLight.on == false && dLight.on == true){
-		lights[0] = dLight;
-		numLight = 1;
-	}
-	else
-		numLight = 0;
-
-	vec3 totalLighting = vec3(aColor) * vec3(mat[m].ambient);
-	vec3 lightDir;
-
-	for(int i = 0; i < numLight; i++){
-		attenuation = 1.0;
-		if (lights[i].position.w == 0.0) // directional light
-			{
-			lightDir = normalize(vec3(lights[i].position));
-			}
-		else // point or spot light //60
-			{
-			lightDir = normalize(vec3(lights[i].position -vPos));
-			}
-		
-
-		vec3 ambientC = lights[i].ambient * mat[m].ambient;
-
-		float diff = max(dot(vNormal, lightDir), 0.0);
-		vec3 diffuseReflection = lights[i].diffuse * (diff * mat[m].diffuse);
-
-		vec3 specularReflection;
-		if(dot(vNormal, lightDir)>0){
-		vec3 viewDir = normalize(camPos - vPos);
-		vec3 halfDir = normalize( lightDir + camPos );  
-		float sp = pow(max(dot(vNormal,halfDir), 0.0), mat[m].shine);
-		specularReflection = lights[i].specular * (sp * mat[m].specular); 
-		}
-		else
-			specularReflection = vec3(0.0, 0.0, 0.0);
-		
-		totalLighting = totalLighting + ambientC + diffuseReflection + specularReflection;
-		}
-
-	color = vec4(totalLighting, 1.0);
+    
+	color = vec4(1.0f, 0.0f, 0.0f, 1.0f);
 }
